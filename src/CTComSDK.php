91454853b25bb9e78962999dc7d0839d83b5a95e
koxon <nmenciere@sportarchive.tv>
2014-05-02 00:21:07 -0400

    basic polling working

--- src/CTComSDK.php
+++ src/CTComSDK.php
@@ -11,22 +11,23 @@ class CTComSDK
     private $aws;
     private $sqs;
 
-    function __construct($key, $secret)
+    function __construct($key = false, $secret = false, $region = false)
     {
-        if (!$key && !$secret)
-        {
-            if (!getenv("AWS_ACCESS_KEY_ID") || !getenv("AWS_SECRET_KEY"))
-                throw new Exception("Provide AWS 'key' and 'secret'!");
-            else {
-                $key    = getenv("AWS_ACCESS_KEY_ID");
-                $secret = getenv("AWS_SECRET_KEY");
-            }
-        }
+        if (!$key)
+            if (!($key = getenv("AWS_ACCESS_KEY_ID")))
+                throw new Exception("Provide AWS 'key'!");
+        if (!$secret)
+            if (!($secret = getenv("AWS_SECRET_KEY")))
+                throw new Exception("Provide AWS 'secret'!");
+        if (!$region)
+            if (!($region = getenv("AWS_REGION")))
+                throw new Exception("Provide AWS 'region'!");
         
         // Create AWS SDK instance
         $this->aws = Aws::factory(array(
                 'key'    => $key,
-                'secret' => $secret
+                'secret' => $secret,
+                'region' => $region
             ));
         
         // SQS Client
@@ -38,38 +39,57 @@ class CTComSDK
         echo("[$type] $message\n");
     }
     
-    public function poll_incoming_msg($queue)
+    public function receive_message($queue, $timeout)
     {
+        // Check the queue provided
+        // If not existing, we create it!
         try {
             $queueUrl = $this->sqs->getQueueUrl(['QueueName' => $queue])['QueueUrl'];
         } catch (\Aws\Sqs\Exception\SqsException $e) {
-            log_out(
-                "ERROR", 
+            $this->log_out(
+                "WARNING", 
                 "Can't get SQS queue '$queue' ! Creating queue '$queue' ..."
             );
             
             try {
-                $newQueue = $sqs->createQueue(['QueueName' => $queue]);
+                $newQueue = $this->sqs->createQueue(['QueueName' => $queue]);
                 $queueUrl = $newQueue['QueueUrl'];
             } catch (\Aws\Sqs\Exception\SqsException $e) {
-                log_out(
+                $this->log_out(
                     "ERROR", 
-                    "Unable to create the new queue '$queue' ... skipping"
+                    "Unable to create the new queue '$queue' ..."
                 );
                 return false;
             }
         }
 
-        while (42)
-        {
-            $result = $this->sqs->receiveMessage(array(
-                    'QueueUrl'        => $queueUrl,
-                    'WaitTimeSeconds' => 5,
-                ));
+        // Loop for message 
+        $result = $this->sqs->receiveMessage(array(
+                'QueueUrl'        => $queueUrl,
+                'WaitTimeSeconds' => $timeout,
+            ));
 
-            $messages = $result->get('Messages');
-            if ($messages && count($messages)) 
-                return $messages;
+        if ($messages = $result->get('Messages'))
+            return $messages;
+    }
+
+    public function delete_message($queue, $msg)
+    {
+        try {
+            $queueUrl = $this->sqs->getQueueUrl(['QueueName' => $queue])['QueueUrl'];
+        } catch (\Aws\Sqs\Exception\SqsException $e) {
+            $this->log_out(
+                "ERROR", 
+                "Can't delete message from an invalid queue '$queue'.\n"
+                . $e->getMessage()
+            );
+            return false;
         }
+
+        $this->sqs->deleteMessage(array(
+                'QueueUrl'        => $queueUrl,
+                'ReceiptHandle'   => $msg['ReceiptHandle']));
+
+        return true;
     }
 }
