4ddf86d2f68397cc7d0bed224b0d1c899dd0995e
koxon <nmenciere@sportarchive.tv>
2014-05-03 01:49:21 -0400

    add IAM and entitlements to SDK

--- src/CTComSDK.php
+++ src/CTComSDK.php
@@ -4,34 +4,81 @@ require __DIR__ . "/../vendor/autoload.php";
 
 // Amazon libraries
 use Aws\Common\Aws;
-use Aws\Swf\Exception;
+use Aws\Sqs;
+use Aws\Sts;
 
 class CTComSDK
 {
+    private $region;
     private $aws;
     private $sqs;
+    private $sts;
+    private $credentials;
+    private $assumedRole;
 
     function __construct($key = false, $secret = false, $region = false)
     {
-        if (!$key)
-            if (!($key = getenv("AWS_ACCESS_KEY_ID")))
-                throw new Exception("Provide AWS 'key'!");
-        if (!$secret)
-            if (!($secret = getenv("AWS_SECRET_KEY")))
-                throw new Exception("Provide AWS 'secret'!");
-        if (!$region)
-            if (!($region = getenv("AWS_REGION")))
-                throw new Exception("Provide AWS 'region'!");
+        /* if (!$key) */
+        /*     if (!($key    = getenv("AWS_ACCESS_KEY_ID"))) */
+        /*         throw new Exception("Provide AWS 'key'!"); */
+        /* if (!$secret) */
+        /*     if (!($secret = getenv("AWS_SECRET_KEY"))) */
+        /*         throw new Exception("Provide AWS 'secret'!"); */
+        /* if (!$region) */
+        /*     if (!($region = getenv("AWS_REGION"))) */
+        /*         throw new Exception("Provide AWS 'region'!"); */
         
+        $this->region = $region;
+
         // Create AWS SDK instance
         $this->aws = Aws::factory(array(
                 'key'    => $key,
                 'secret' => $secret,
                 'region' => $region
             ));
+        $this->sts = $this->aws->get('Sts');
+    }
+
+    private function init_sqs_client($client)
+    {
+        if (isset($this->assumedRole) && $this->assumedRole)
+        {
+            print_r($this->assumedRole);
+        }
+
+        try {
+            $role       = $client["SQS"]["role"];
+            $externalId = $client["SQS"]["externalId"];
+            $assume = array(
+                'RoleArn'         => $role,
+                'RoleSessionName' => time()."-".$client["client"],
+                'DurationSeconds' => 900
+            );
+            if ($externalId && $externalId != "")
+                $assume['ExternalId'] = $externalId;
+            
+            // Get TMP credentials
+            $this->assumedRole = $this->sts->assumeRole($assume);
+        } catch (\Aws\Sts\Exception\StsException $e) {
+            $this->log_out("ERROR", $e->getMessage());
+            return false;
+        }
         
-        // SQS Client
-        $this->sqs = $this->aws->get('Sqs');
+        try {
+            $this->credentials = 
+                $this->sts->createCredentials($this->assumedRole);
+
+            // SQS Client
+            $this->sqs = Sqs\SqsClient::factory(array(
+                    'credentials' => $this->credentials,
+                    'region'      => $this->region
+                ));
+        } catch (\Aws\Sqs\Exception\SqsException $e) {
+            $this->log_out("ERROR", $e->getMessage());
+            return false;
+        }
+        
+        return true;
     }
 
     private function log_out($type, $message)
@@ -39,57 +86,44 @@ class CTComSDK
         echo("[$type] $message\n");
     }
     
-    public function receive_message($queue, $timeout)
+    public function receive_message($client, $timeout)
     {
-        // Check the queue provided
-        // If not existing, we create it!
+        if (!$this->init_sqs_client($client))
+            return false;
+ 
         try {
-            $queueUrl = $this->sqs->getQueueUrl(['QueueName' => $queue])['QueueUrl'];
-        } catch (\Aws\Sqs\Exception\SqsException $e) {
-            $this->log_out(
-                "WARNING", 
-                "Can't get SQS queue '$queue' ! Creating queue '$queue' ..."
-            );
-            
-            try {
-                $newQueue = $this->sqs->createQueue(['QueueName' => $queue]);
-                $queueUrl = $newQueue['QueueUrl'];
-            } catch (\Aws\Sqs\Exception\SqsException $e) {
-                $this->log_out(
-                    "ERROR", 
-                    "Unable to create the new queue '$queue' ..."
-                );
-                return false;
-            }
+            // Loop for message 
+            $queue = $client["SQS"]["output"];
+            $result = $this->sqs->receiveMessage(array(
+                    'QueueUrl'        => $queue,
+                    'WaitTimeSeconds' => $timeout,
+                ));
         }
-
-        // Loop for message 
-        $result = $this->sqs->receiveMessage(array(
-                'QueueUrl'        => $queueUrl,
-                'WaitTimeSeconds' => $timeout,
-            ));
-
+        catch (\Aws\Sqs\Exception\SqsException $e) {
+            $this->log_out("ERROR", $e->getMessage());
+            sleep($timeout);
+            return false;
+        }
+        
         if ($messages = $result->get('Messages'))
             return $messages;
     }
 
-    public function delete_message($queue, $msg)
+    public function delete_message($client, $msg)
     {
+        if (!$this->init_sqs_client($client))
+            return false;
+        
         try {
-            $queueUrl = $this->sqs->getQueueUrl(['QueueName' => $queue])['QueueUrl'];
+            $queue = $client["SQS"]["output"];
+            $this->sqs->deleteMessage(array(
+                    'QueueUrl'        => $queue,
+                    'ReceiptHandle'   => $msg['ReceiptHandle']));
         } catch (\Aws\Sqs\Exception\SqsException $e) {
-            $this->log_out(
-                "ERROR", 
-                "Can't delete message from an invalid queue '$queue'.\n"
-                . $e->getMessage()
-            );
+            $this->log_out("ERROR", $e->getMessage());
             return false;
         }
-
-        $this->sqs->deleteMessage(array(
-                'QueueUrl'        => $queueUrl,
-                'ReceiptHandle'   => $msg['ReceiptHandle']));
-
+        
         return true;
     }
 }
